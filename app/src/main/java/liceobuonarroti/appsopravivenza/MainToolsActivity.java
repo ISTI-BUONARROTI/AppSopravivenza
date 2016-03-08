3d7f1ec78812a068c6895d0e5ad0cbbae2dca15d
Giorgio <imatesiu@gmail.com>
2016-03-08 12:30:31 +0100

    prima versione

---
+++ app/src/main/java/liceobuonarroti/appsopravivenza/MainToolsActivity.java
@@ -0,0 +1,13 @@
+package liceobuonarroti.appsopravivenza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainToolsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tools);
+    }
+}
